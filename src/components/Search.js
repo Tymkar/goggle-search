7c7fbf6c98dd7258b50749326d92b8b3c6132c88
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-25 21:00:57 +0200

    Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { Links } from "./Links";
+import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+import { useResultContext } from "../contexts/ResultContextProvider";
 
 export const Search = () => {
+  const [text, setText] = useState("plants");
+  const { setSearchTerm } = useResultContext();
+  const [debouncedValue] = useDebounce(text, 300);
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setSearchTerm(debouncedValue);
+    }
+  }, [debouncedValue]);
+
   return (
-    <div>
-      Search
+    <div className="realtive sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+      <input
+        value={text}
+        type="text"
+        className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        placeholder="Search"
+        onChange={(e) => setText(e.target.value)}
+      />
+      {!text && (
+        <button
+          type="button"
+          className="absolue top-1.5 right-4 text-2xl text-gray-500"
+          onClick={() => setText("")}
+        >
+          X
+        </button>
+      )}
       <Links />
     </div>
   );
