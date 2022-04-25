81cd0381db11e54822cb6ef61d8f6c5146031065
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-25 08:15:58 +0200

    base styles and components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import { Routes } from "./components/Routes";
 import { Footer } from "./components/Footer";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-      <h1 className="font-bold underline">App</h1>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 };
