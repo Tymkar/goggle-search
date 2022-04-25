9abb34dd6515116d52c7dedf6f481c576bff7c62
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-25 21:19:55 +0200

    set default string search term value

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const BaseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("bacon");
+  const [searchTerm, setSearchTerm] = useState("Cristiano Ronaldo");
 
   const getResults = async (type) => {
     setIsLoading(true);
