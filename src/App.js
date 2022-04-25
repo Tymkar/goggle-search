1f6dd7f89b14f3937a3d1ae2fe9961d98ae86a8b
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-25 13:40:53 +0200

    Routes bugs fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
-import { Routes } from "./components/Routes";
+import { TheRoutes } from "./components/TheRoutes";
 import { Footer } from "./components/Footer";
 
 const App = () => {
@@ -15,7 +15,7 @@ const App = () => {
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
         <Navbar darkTheme={darkTheme} changeTheme={changeTheme} />
-        <Routes />
+        <TheRoutes />
         <Footer />
       </div>
     </div>
