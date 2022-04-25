015617c877bc8614704f0b36f4612292083bf45f
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-25 21:00:40 +0200

    add conditional video rendering

--- src/components/Results.js
+++ src/components/Results.js
@@ -87,13 +87,14 @@ export const Results = () => {
         <div className="flex flex-wrap">
           {results.map((video, index) => (
             <div key={index} className="p-2">
-              {console.log(video.additional_links)}
-              <ReactPlayer
-                url={video.additional_links?.[0].href}
-                controls
-                width="355px"
-                height="200px"
-              />
+              {video?.additional_links?.[0]?.href && (
+                <ReactPlayer
+                  url={video.additional_links?.[0].href}
+                  controls
+                  width="355px"
+                  height="200px"
+                />
+              )}
             </div>
           ))}
         </div>
